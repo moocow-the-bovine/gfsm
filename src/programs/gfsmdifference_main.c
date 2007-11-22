20a7e0eccdf9e1130c734cbc2db99135876ca2cd
Bryan Jurish <jurish@bbaw.de>
2007-11-22 23:11:59 +0000

    + testsuite work: found bug in difference
    git-svn-id: svn+ssh://svn.dwds.de/home/moocow/svn/public/gfsm/trunk@422 097f6656-c427-11dc-a790-1b5d5187385a

--- src/programs/gfsmdifference_main.c
+++ src/programs/gfsmdifference_main.c
@@ -82,7 +82,7 @@ void compute_operation(const char *infilename)
     exit(255);
   }
 
-  //-- compute differenceion
+  //-- compute difference
   if (fsmOut == NULL) {
     fsmOut = fsmIn;
     fsmIn  = gfsm_automaton_new();
