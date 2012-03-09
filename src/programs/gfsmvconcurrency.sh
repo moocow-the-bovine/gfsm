7e0ec653a7bc3e32e8ce48b44c5fcad4d05f2a5b
Bryan Jurish <jurish@bbaw.de>
2012-03-09 09:19:12 +0000

    * updated gfsmvconcurrency.sh (todo: move this stuff to perl)
    git-svn-id: svn+ssh://svn.dwds.de/home/moocow/svn/public/gfsm/trunk@684 097f6656-c427-11dc-a790-1b5d5187385a

--- src/programs/gfsmvconcurrency.sh
+++ src/programs/gfsmvconcurrency.sh
@@ -1,15 +1,55 @@
 #!/bin/bash
 
-gfsmfile="$1"
-gfsmshort=$(basename "$gfsmfile")
+args=()
+datamode=""
+doplot="y"
+
+while test $# -gt 0 ; do
+  a="$1"
+  shift
+  case "$a" in
+    -data|--data|-d)
+      datamode=y
+      ;;
+    -plot|--plot|-p)
+      doplot=y
+      ;;
+    -noplot|-no-plot|--noplot|--no-plot|-P)
+      doplot=""
+      ;;
+    *)
+      args=("${args[@]}" "$a")
+      ;;
+  esac
+done
+
+test -n "$doplot" && plotcmd=(gnuplot -persist) || plotcmd=(cat)
 gfsmconcurrency=$(dirname $0)/gfsmconcurrency.sh
 
+if test "${#args[@]}" -eq 0 ; then
+  args=("-")
+fi
+
 (
-cat <<EOF;
+ cat <<EOF;
   set xlabel "Depth";
   set ylabel "Number of States";
-  set title "Concurrency / $gfsmshort";
-  plot "-" w lp title "$gfsmshort";
+  set title "States by Depth";
+  set style data l;
 EOF
-$gfsmconcurrency -v $gfsmfile
-) | gnuplot -persist
+ echo -n " plot ";
+ i=0 ;
+ while test $i -lt ${#args[@]} ; do
+    f="${args[$i]}";
+    test $i -eq 0 || echo -n ", ";
+    echo -n "\"-\" title \"$(basename $f)\"";
+    let "i=$i+1";
+ done
+ echo -e ";\n"
+  ##
+  ##-- plot data
+ for f in "${args[@]}" ; do
+     test -n "$datamode" && cat "$f" || $gfsmconcurrency -v $f
+     echo "e"
+ done
+) | "${plotcmd[@]}"
